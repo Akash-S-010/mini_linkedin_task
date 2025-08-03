f72687a6ad8df90dceb939e76134691a0487d674
Akash S <akashspalloor@gmail.com>
2025-08-03 20:11:48 +0530

    updated cors origin to new client url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({ 
-    origin: "http://localhost:5173",
+    origin: "https://mini-linkedin-task.vercel.app",
     credentials: true,
 }));
 
